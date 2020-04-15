f3eb05364d0040b8e55bdd42c8ea6e30a7465c37
Chris Davis <chrisdavis0375@gmail.com>
2020-04-15 18:49:55 -0500

    Dice functionality complete and responsive styling added

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -16,12 +16,10 @@ var scores, roundScore, activePlayer, dice, gamePlaying;
 newGame();
 
 function newGame() {
-  scores = [0, 0];
+  scores = [0, 0, 0];
   roundScore = 0;
   activePlayer = 1;
   gamePlaying = true;
-
-  document.querySelector(".dice").style.display = "none";
 
   document.getElementById("score-1").textContent = "0";
   document.getElementById("score-2").textContent = "0";
@@ -43,34 +41,68 @@ document.querySelector(".btn-new").addEventListener("click", function () {
 });
 
 document.querySelector(".btn-roll").addEventListener("click", function () {
-  console.log("Dice Rolled.");
-  // Random numbers for dice
-  var dice1 = Math.floor(Math.random() * 6) + 1;
-  var dice2 = Math.floor(Math.random() * 6) + 1;
+  if (gamePlaying) {
+    console.log("Dice Rolled.");
+    // Random numbers for dice
+    var dice1 = Math.floor(Math.random() * 6) + 1;
+    var dice2 = Math.floor(Math.random() * 6) + 1;
 
-  // Update UI
-  document.getElementById("dice-1").style.display = "block";
-  document.getElementById("dice-2").style.display = "block";
-  document.getElementById("dice-1").src =
-    "./assets/images/dice-" + dice1 + ".png";
-  document.getElementById("dice-2").src =
-    "./assets/images/dice-" + dice2 + ".png";
+    // Update UI
+    document.getElementById("dice-1").style.display = "block";
+    document.getElementById("dice-2").style.display = "block";
+    document.getElementById("dice-1").src =
+      "./assets/images/dice-" + dice1 + ".png";
+    document.getElementById("dice-2").src =
+      "./assets/images/dice-" + dice2 + ".png";
 
-  if (dice1 !== 1 && dice2 !== 1) {
-    roundScore += dice1 + dice2;
+    if (dice1 !== 1 && dice2 !== 1) {
+      roundScore += dice1 + dice2;
 
-    document.getElementById("current-" + activePlayer).textContent = roundScore;
-  } else {
-    nextPlayer();
+      document.querySelector(
+        "#current-" + activePlayer
+      ).textContent = roundScore;
+    } else {
+      nextPlayer();
+    }
   }
 });
 function nextPlayer() {
   activePlayer === 1 ? (activePlayer = 2) : (activePlayer = 1);
+  console.log("Player " + activePlayer + "'s turn");
+  roundScore = 0;
 
   document.getElementById("current-1").textContent = "0";
   document.getElementById("current-2").textContent = "0";
+
   document.querySelector(".player-1-panel").classList.toggle("active");
   document.querySelector(".player-2-panel").classList.toggle("active");
-
-  document.querySelector(".dice").style.display = "none";
 }
+
+document.querySelector(".btn-hold").addEventListener("click", function () {
+  if (gamePlaying) {
+    scores[activePlayer] += roundScore;
+
+    console.log(scores);
+
+    document.querySelector("#score-" + activePlayer).textContent =
+      scores[activePlayer];
+
+    var winningScore = 100;
+
+    if (scores[activePlayer] >= winningScore) {
+      document.getElementById("player-" + activePlayer).textContent = "Winner!";
+
+      document.querySelector(".dice").style.display = "none";
+
+      document
+        .querySelector(".player-" + activePlayer + "-panel")
+        .classList.add("winner");
+      document
+        .querySelector(".player-" + activePlayer + "-panel")
+        .classList.remove("active");
+      gamePlaying = false;
+    } else {
+      nextPlayer();
+    }
+  }
+});
